c7886d1b74f756e56fb5b50ae21fe3b9478723a2
Nirjhar Debnath <nirjhardebnath2006@gmail.com>
2026-05-07 19:27:35 +0530

    Create subjects.py

---
+++ utils/subjects.py
@@ -0,0 +1,65 @@
+subjects = {
+    # ================= SEMESTER 1 =================
+    "BS-M101": "Mathematics-IA",
+    "BS-PH101": "Physics-I",
+    "ES-EE101": "Basic Electrical Engineering",
+    "BS-PH191": "Physics-I Laboratory",
+    "ES-EE191": "Basic Electrical Engineering Lab",
+    "ES-ME192": "Workshop",
+
+    # ================= SEMESTER 2 =================
+    "BS-CH201": "Chemistry-I",
+    "BS-M201": "Mathematics-IIA",
+    "ES-CS201": "Programming for Problem Solving",
+    "HM-HU201": "English",
+    "BS-CH291": "Chemistry-I Laboratory",
+    "ES-CS291": "Programming for Problem Solving Lab",
+    "ES-ME291": "Engineering Graphics & Design",
+    "HM-HU291": "Language Laboratory",
+
+    # ================= SEMESTER 3 =================
+    "ESC301": "Analog and Digital Electronics",
+    "PCC-CS301": "Data Structure & Algorithms",
+    "PCC-CS302": "Computer Organization",
+    "BSC301": "Mathematics-III",
+    "HSMC301": "Economics for Engineers",
+    "PCC-CS391": "Data Structure & Algorithm Lab",
+    "PCC-CS392": "Computer Organization Lab",
+
+    # ================= SEMESTER 4 =================
+    "PCC-CS401": "Design & Analysis of Algorithms",
+    "PCC-CS402": "Database Management Systems",
+    "PCC-CS403": "Discrete Mathematics",
+    "PCC-CS404": "Formal Language & Automata Theory",
+    "PCC-CS491": "Database Management Systems Lab",
+    "PCC-CS492": "Discrete Mathematics Lab",
+    "PCC-CS493": "Formal Language & Automata Theory Lab",
+    "PCC-CS494": "Design & Analysis Algorithm Lab",
+
+    # ================= SEMESTER 5 =================
+    "ESC501": "Software Engineering",
+    "PCC-CS501": "Compiler Design",
+    "PCC-CS502": "Operating Systems",
+    "PCC-CS503": "Object Oriented Programming",
+    "ESC591": "Software Engineering Lab",
+    "PCC-CS592": "Operating Systems Lab",
+    "PCC-CS593": "Object Oriented Programming Lab",
+
+    # ================= SEMESTER 6 =================
+    "PCC-CS601": "Computer Networks",
+    "PCC-CS602": "Machine Learning",
+    "PCC-CS603": "Cryptography and Network Security",
+    "PCC-CS691": "Machine Learning Lab",
+    "PCC-CS692": "Computer Networks Lab",
+
+    # ================= SEMESTER 7 =================
+    "OEC-CS701A": "Operation Research",
+    "OEC-CS701B": "Multimedia Technology",
+    "PEC-CS701E": "Machine Learning Advanced Topics",
+    "HSMC701": "Entrepreneurship Development",
+    "PROJ-CS781": "Project-II",
+
+    # ================= SEMESTER 8 =================
+    "PROJ-CS881": "Project / Dissertation",
+    "INT-CS881": "Internship"
+}
